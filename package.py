e67579b64ceddcfa7767b969bd221659e68b25a5
splidje <jonathan.hearn@ajptechnical.com>
2026-07-07 17:57:33 +0100

    tagged 0.3.1

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "ubp_nuke_turnover"
 title = "UBP Nuke Turnover"
-version = "0.3.0"
+version = "0.3.1"
 client_dir = "ayon_ubp_nuke_turnover"
 ayon_required_addons = {
     "nuke": ">=0.3.18",
